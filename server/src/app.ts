6fc41a5308aab9da66ea8e5a1ea34d6bfe6f40df
mert18 <mertuygur02@gmail.com>
2021-09-27 18:10:29 +0300

    server setup client setup

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { createServer } from "http";
+import config from "config";
+import logger from "./utils/logger";
+import { version } from "../package.json";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+
+const app = express();
+
+const httpServer = createServer(app);
+
+app.get("/", (_, res) =>
+  res.send(`Server is up and running version ${version}`)
+);
+
+httpServer.listen(port, host, () => {
+  logger.info(`🚀 Server version ${version} is listening 🚀`);
+  logger.info(`http://${host}:${port}`);
+});
