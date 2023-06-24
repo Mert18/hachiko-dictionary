9c0589022826108fbeb2aaf43f0bc3681bed0f75
mert18 <mertuygur02@gmail.com>
2023-06-24 12:19:33 +0300

    21: auth redesigned.

--- app/main/layout.js
+++ app/main/layout.js
@@ -17,17 +17,20 @@ const MainLayout = ({ children }) => {
     context.setUser({});
     localStorage.clear();
     axiosInstance.defaults.headers.common["Authorization"] = "";
-    window.location.href = "/authentication";
+    window.location.reload();
   };
 
   return (
     <div className="grid grid-cols-12">
-      <div className="col-start-1 col-end-13 md:col-start-1 md:col-end-7 bg-white flex justify-center items-start pt-20 min-h-screen">
+      <div
+        className="col-start-1 col-end-13 md:col-start-1 md:col-end-7 bg-white flex justify-center items-start pt-20 min-h-screen">
         {children}
       </div>
-      <div className="col-start-1 col-end-13 md:col-start-7 md:col-end-13 bg-primary flex flex-col md:fixed md:right-0 md:top-0 md:w-1/2 md:h-screen">
+      <div
+        className="col-start-1 col-end-13 md:col-start-7 md:col-end-13 bg-primary flex flex-col md:fixed md:right-0 md:top-0 md:w-1/2 md:h-screen">
         <div className="flex flex-col md:flex-1 bg-primary relative">
-          <div className="absolute top-0 right-0 m-2 bg-white rounded-md flex justify-center items-center hover:bg-red-100 hover:cursor-pointer">
+          <div
+            className="absolute top-0 right-0 m-2 bg-white rounded-md flex justify-center items-center hover:bg-primary-400 hover:cursor-pointer">
             <button className="p-2" onClick={() => handleLogout()}>
               <Image
                 src="/icons/logout.svg"
